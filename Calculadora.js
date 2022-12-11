3a7020236f319f92688a87f768313c82ae2eb7cf
juan pita <neos961@gmail.com>
2022-12-11 21:47:10 +0100

    first commit

---
+++ Calculadora.js
@@ -0,0 +1,17 @@
+class Calculadora {
+    sumar(num1, num2) {
+        return num1 + num2;
+    }
+
+    restar(num1, num2) {
+        return num1 - num2;
+    }
+
+    division(num1, num2) {
+        return num1 / num2;
+    }
+
+    multiplicar(num1, num2) {
+        return num1 * num2;
+    }
+}
